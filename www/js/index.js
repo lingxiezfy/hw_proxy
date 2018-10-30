04ef06822e5fa0d0290be61c1c9d7cf3ab02c5d2
FrankZ <1241948182@qq.com>
2018-10-30 16:38:34 +0800

    修复弹窗无滚动条问题

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 function openScan() {
     var scrWidth=screen.availWidth;
     var scrHeight=screen.availHeight;
-    var win =window.open('statusscan/index.html','statusscan','height=100, width=100, top=0,left=0,toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no');
+    var win =window.open('statusscan/index.html','statusscan','height=100, width=100, top=0,left=0,toolbar=no, menubar=no, scrollbars=yes, resizable=no, location=no, status=no');
     win.moveTo(0,0);
     win.resizeTo(scrWidth,scrHeight);
 }
