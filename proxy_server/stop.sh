2f03926c43f957feb0fdcc7953c7625b6596728b
FrankZ <1241948182@qq.com>
2018-10-31 16:55:32 +0800

    修改proxyserver文件名，并修改相应启动脚本

--- proxy_server/stop.sh
+++ proxy_server/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill -s 9 `ps -ef | grep proxy_server/reactor_proxy_server | grep -v grep | awk '{print $2}'`
+kill -s 9 `ps -ef | grep proxy_server/proxy_server | grep -v grep | awk '{print $2}'`
